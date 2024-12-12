a1fa90dedd52943a62a38e46cc6ac9e689c264dd
Wai Yan Min <waiyanmin.lane@gmail.com>
2024-12-12 11:46:23 -0800

    connection mongodb using mongoose

---
+++ index.js
@@ -0,0 +1,30 @@
+const mongoose= require("mongoose");
+const Campsite= require("./models/campsite");
+
+const url="mongodb://localhost:27017/nucampsite";
+const connect =mongoose.connect(url,{});
+
+connect.then(()=>{
+    console.log("Connected correctly to the server");
+
+    const newCampsite = new Campsite({
+        name:"React Lake Campground",
+        description:"test"
+    });
+    newCampsite.save()
+    .then(campsite=>{
+        console.log(campsite);
+        return Campsite.find();
+    })
+    .then(campsites=>{
+        console.log(campsites);
+        return Campsite.deleteMany();
+    })
+    .then(()=>{
+        return mongoose.connection.close();
+    })
+    .catch(()=>{
+        console.log(err);
+        return mongoose.connection.close();
+    });
+});
